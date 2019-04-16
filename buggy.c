5248d2b59019a221b7903533dbd3ff862fcbfad3
Rookal <41110579+Rookal@users.noreply.github.com>
2019-04-15 21:37:23 -0500

    Add files via upload

--- buggy.c
+++ buggy.c
@@ -7,31 +7,30 @@
 3. create a new branch on git
 4. git push changed buggy.c
 5. create pull request with your name and changes you made
-
 */
 
 struct dog
 {
 	int age;
-	char name = [10];
+	char name[13]; //Changed bracket type and removed = sign
 };
 
-
-void main()
+int main()
 {
-	struct (dog) thing1:
+	struct dog thing1; //Semi colon
 	struct dog thing2;
 
-	thing1.age = [5];
+	thing1.age = 5;
 	thing2.age = 13;
-	
-	puts{"What is your dogs name? "};
-	gets(thing1.names);
 
-	strcpy(thing2.name, "Buster");
+	puts("What is your dogs name?"); //Changed to regular brackets
+	scanf("%s", thing1.name); //Removed S in names, changed to scanf_s because it was not working nice with gets
 
-	printf("Name: %i\n",thing1.name );
-	printf("Age: %i\n",thing1.age):
-	printf("Thing2 Name: %s\n,thing2.name);
-	printf("Thing2 Age %i\n',thing2.age);
+	strcpy_s(thing2.name, "Buster");
+
+	printf("Name: %s\n", thing1.name); // Changed to %s
+	printf("Age: %i\n", thing1.age); //Semi colon
+	printf("Thing2 Name: %s\n", thing2.name); //End Quotation + Rearrange comma
+	printf("Thing2 Age %i\n'", thing2.age); //End Quotation + Rearrange comma
+
 }
